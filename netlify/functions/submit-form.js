1918ce526b6bf8d80e6d4d94b0f6e21b980fdccc
leeeeesamo <lmoore0510@gmail.com>
2025-01-03 14:28:39 -0600

    Updated site layout, added new images, and reorganized files

---
+++ netlify/functions/submit-form.js
@@ -0,0 +1,19 @@
+// netlify/functions/submit-form.js
+
+exports.handler = async function(event, context) {
+    if (event.httpMethod !== 'POST') {
+        return {
+            statusCode: 405,
+            body: JSON.stringify({ message: "Method Not Allowed" })
+        };
+    }
+
+    const formData = JSON.parse(event.body);
+
+    console.log("Form Data:", formData);
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Form submission successful!" })
+    };
+};
