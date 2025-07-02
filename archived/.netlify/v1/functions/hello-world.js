8b8ebe6af75086b8910f878df4b8a868378d387a
leeeeesamo <lmoore0510@gmail.com>
2025-07-02 16:26:48 -0500

    updated files

--- archived/.netlify/v1/functions/hello-world.js
+++
@@ -1,7 +0,0 @@
-// v1/functions/hello-world.js
-exports.handler = async function(event, context) {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Hello, World!" })
-    };
-};
